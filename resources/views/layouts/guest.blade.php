03dc285ef7930be04620f9e6f8a3eba77b3f5c19
eedgmnurtech <edegracia@nurtech.org>
2026-01-08 22:40:44 -0500

    Add favicon to guest layout for improved branding and user experience.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,6 +7,8 @@
 
         <title>{{ config('app.name', 'Mi Finca') }}</title>
 
+        <link rel="icon" href="{{ asset('mi-finca.ico') }}" type="image/x-icon">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
